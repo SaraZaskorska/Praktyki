115bda9f7927898bfeeed3b1f61386696d568f15
SaraZaskorska <89976400+SaraZaskorska@users.noreply.github.com>
2023-11-21 16:54:25 +0100

    Update script.js

--- 13.11/zadanie 6,7,8/script.js
+++ 13.11/zadanie 6,7,8/script.js
@@ -7,33 +7,77 @@ const books = [
     {title: 'World Ender 3', pages: 199, genre: 'fantasy', rating: 1},
     {title: 'Paranormal', pages: 200, genre: 'thriller', rating: 5},
 ];
+//zadanie6
+  const filtr = (list) => list.filter((book) => book.pages%2 == 0 && book.genre.endsWith("y"));
+  const suma = (book) => book.reduce((razem, book) => razem + book.title.replace(/\s/g, '').length, 0);
+  const compose = (...fns) => (x) => fns.reduce((acc, fn) => fn(acc), x);
+  let wynik = compose(filtr, suma);
 
-const zadanie6 = () => {
-    let wynik = 0;
-    for (const book of books) {
-        if (book.pages % 2 === 0 && book.genre.endsWith('y')) {
-            const tytulBezSpacji = book.title.replace(/\s/g, '');
-            wynik += tytulBezSpacji.length;
-        }
-    }
-    console.log(wynik);
-};
+console.log("Zadanie 6: " + wynik(books));
 
-const zadanie7 = () => {
-    let wynik = 0;
-    for (const book of books) {
-        if (book.pages % 2 !== 0 && /\d/.test(book.title) && book.rating > 5) {
-            wynik++;
-        }
-    }
-    console.log(wynik);
-};
+//zadanie 7
+  const filtr1 = (list) => list.filter((book) => book.rating > 5 && book.pages%2 == 1 && /\d+/.test(book.title));
+  const suma1 = (book) => book.reduce((rating) => rating + 1, 0);
+  const compose1 = (...fns) => (x) => fns.reduce((acc, fn) => fn(acc), x);
+  let wynik1 = compose1(filtr1, suma1);
 
-const zadanie8 = () => {
+  console.log("Zadanie 7: " + wynik1(books));
+//zadanie8
     const mapper = (arr) => arr.map(({title}) => title);
     const sorter = (arr) => arr.sort((a, b) => (a.length < b.length) - (a.length > b.length))
-    
-    const compose = (...fns) => (x) => fns.reduceRight((acc, fn) => fn(acc), x)
+    const compose2 = (...fns) => (x) => fns.reduceRight((acc, fn) => fn(acc), x)
+    let wynik2 = compose2(sorter, mapper)(books)[1]
+    console.log("zadanie 8: "+ wynik2)
 
-    console.log(compose(sorter, mapper)(books)[1])
-};
+//zadanie 9
+    const promise = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('Udało się!');
+      }, 5000);
+    });
+
+    promise.then((result) => {
+      console.log(result);
+    });
+
+
+  
+//zadanie10
+    const liczenie = (x, y) => {
+      return new Promise((resolve, reject) => {
+        if (typeof x !== 'number' || typeof y !== 'number') {
+          reject(new Error('Jeden z argumentów nie jest liczbą!'));
+        } else {
+          const result = x * y;
+          resolve(result);
+        }
+      });
+    };
+
+    liczenie(5, 10)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  
+//zadanie11
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error('Błąd! Status nie jest równy 200');
+        }
+      })
+      .then((data) => {
+        data.forEach(({ title, body }) => {
+          console.log('Tytuł:', title);
+          console.log('Body:', body);
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  
